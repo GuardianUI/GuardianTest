1aeca027a3917e5f07330eddf8676f558a0379cc
ivelin <ivelin.eth@gmail.com>
2023-05-31 14:38:30 +0000

    fix: playwright config - no parallel runs
    Signed-off-by: ivelin <ivelin.eth@gmail.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   testDir: "./test-examples",
   testMatch: [/.*\.(spec|test|gui)\.(js|ts|mjs)/],
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
